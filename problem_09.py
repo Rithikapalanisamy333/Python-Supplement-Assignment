658342edd0bf7037c784ef3b6d288004f21e5cf7
Rithikapalanisamy333 <rithikapalanisamy999@gmail.com>
2026-01-30 14:50:45 +0530

    Update problem_09.py

--- problem_09.py
+++ problem_09.py
@@ -2,6 +2,6 @@
 # Find and fix the error
 
 numbers = [10, 20, 30, 40, 50]
-total = sum(numbers)
-average = total / len(numbers)
+Total = sum(numbers)
+average = Total / len(numbers)
 print(f"Average: {average}")
